fce2e71d99bb7363c6535ccdfafa18ebf5d42d5c
Sunip Mukherjee <sunipkmukherjee@gmail.com>
2019-11-19 15:12:51 -0500

    Refer to Readme for changelog, v1.0a

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,109 +1,120 @@
+
+/* Written by Sunip K. Mukherjee (c) 2019 @ UML
+ *  
+ *  Utilizes the Si446x library by Zak Kemble (https://github.com/zkemble/Si446x)
+ *  
+ *  Connection Layout:
+ *  
+ *  ARDUINO              Si446x
+ *  ----------------------------
+ *  3.3V                 Power
+ *  ----                 GPIO0
+ *  ----                 GPIO1
+ *  MISO                 SDO
+ *  MOSI                 SDI
+ *  SCLK                 SCLK
+ *  10                   SPI SS
+ *  2                    NIRQ
+ *  5                    SDN
+ *  GND                  GND
+ *  ============================
+ *  
+ *  ARDUINO              RPi
+ *  ----------------------------
+ *  19 (RX)              14 (TX)
+ *  18 (TX)              15 (RX)
+ *  14 (PIPE_IRQ)        11 (GPIO17)
+ *  GND                  GND
+ *  ============================
+ *  
+ * 9.6 kbps, 2.4 kHz deviation, single channel, 2GFSK (Frequency not finalized)
+ * Packet structure: 0xAAAAAAAAAA | 0x7E | F1 L | F1 (upto 128 bytes) | CRC16 [F1L + F1]
+ * CRC: CRC16 CCITT-False (Init: 0xFFFF), enabled over F1L and F1 and transmitted at end of F1
+ * NO DATA WHITENING
+ * NO MANCHESTER ENCODING
+ * 
+ * This program essentially enables a half-duplex style Serial terminal.
+ * End goal: To enable a PPP over Serial, over which SSH access can be granted.
+ * SSH takes care of packet encryption (RSA-2048)
+ * PPP and SSH not tested (2019-11-19)
+ * 
+ * The program is intended to run on an Arduino Due with dual serial port (USB-Serial or Serial for
+ * data and Serial1 for debug).
+ * 
+ * Pipe mode is enabled using an ISR attached to a GPIO pin.
+ * 
+ * This makes the code easier to maintain. 
+ * 
+ */
+
 #include <Si446x.h>
 #include <string.h>
 
-uint16_t  gen_pack(char * in, size_t len, uint8_t * out) //caller function has to guarantee that out has length == len + 3 (1 for size, 2 for CRC) and also free the allocated memory
-{
-	uint16_t crc = 0xffff ; //init
-	uint16_t poly = 0x1021 ; // CRC Polynomial, 0001 0000 0010 0001 (0, 5, 12)
-	uint8_t lim = in[len - 1] == '\0' ? len - 1 : len ; //change limits to handle null termination properly
-    out [0] = lim ; //prepend packet by message length
-    memcpy( out+1 , in, lim) ; //append message to packet
-	for ( uint8_t i = 0 ; i < lim + 1 ; i++ ) { //increase length by 1 to address the prepended length
-		for ( uint8_t j = 0 ; j < 8 ; j++ ) { //for each bit
-			uint8_t bit = (out[i] >> (7-j) & 1) ;
-			uint8_t c15 = (crc >> 15      & 1) == 1 ? 1 : 0 ;
-			//printf("In CRC: i: %d, j: %d, bit: %d, c15: %d, crc: 0x%x\n", i, j, bit, c15, crc);
-			crc <<= 1 ;
-			if ( c15 ^ bit ) crc ^= poly ;
-		}
-	}
-    crc &= 0xffff; //is this necessary?
-    out [ lim + 1 ] = (uint8_t)(crc>>8) ;
-    out [ lim + 2 ] = (uint8_t)(crc) ;
-    return crc ; //return the CRC for sanity checking
-}
-/*
- * Apparently the radio suppports CRC-ing
- * The code right now has a CRC function, but the packets only include the messages themselves.
- */
-
-#define _BV(bit) (1 << (bit))
+#define _BV(bit) (1 << (bit)) //_BV macro for bit representation
 
 #define PACKET_NONE 0
 #define PACKET_OK 2
 #define MAX_PACKET_SIZE 64
 
-void SI446X_CB_RXCOMPLETE(uint8_t length, int16_t rssi)
+#define PIPE_INTERRUPT 14
+
+volatile byte RECV_STAT = 0 ;
+
+void SI446X_CB_RXCOMPLETE(uint8_t length, int16_t rssi) //Receive interrupt function
 {
   if(length > MAX_PACKET_SIZE)
     length = MAX_PACKET_SIZE;
-  //Serial.print("Received. Len: "); Serial.print(length); Serial.print(" | RSSI: ") ; Serial.println(rssi); 
   char buf[MAX_PACKET_SIZE] ;
-
+  Serial1.print("RSSI: "); Serial1.println(rssi);
   Si446x_read((uint8_t*)buf, length);
+  if ( RECV_STAT ){ // if we are printing the enable message, we want to put it on the debug serial instead of the data serial
+    RECV_STAT-- ; // this is not in the if statement to avoid buffer underflow
+    if ( buf[length-1] != '\r' ) // no need to print the carriage return
+      Serial1.print(buf); // this is affordable because PIPE mode is rarely enabled
+    return ;
+  }
   Serial.print(buf);  
-  if ( buf[length-1] == '\r' )
-    Serial.println();
+  return ;
+}
+
+void EN_PIPE(void) // ISR to enable PIPE mode
+{
+  char cmd [] = "ES+W22003320\r" ; //turn on tunnel mode
+  Serial1.println("Sending PIPE command...") ;
+  Si446x_TX(cmd, strlen(cmd), 0, SI446X_STATE_RX); // transmit the tunnel mode packet and fall back to RX mode
+  RECV_STAT = 8 ; // going to receive 8 bytes (OK+3323\r) that need to be put out of the debug port instead of the data port
   return ;
 }
  
 void setup ()
 {
-    Serial.begin(115200);
-    Serial.println("Init: Serial");
+    attachInterrupt(digitalPinToInterrupt(PIPE_INTERRUPT), EN_PIPE, RISING) ; // Attach the EN_PIPE handler to PIPE_INTERRUPT on the RISING edge
+    Serial.begin(9600); // Data transmission, at 9.6 kbaud to relate to the 9.6 kbaud radio rate
+    Serial1.begin(9600); // Debug transmission
+    Serial1.println("Init: Serial -- Done");
     Si446x_init() ;
-    si446x_info_t info ;
-    Serial.println("Init: Radio");
-    delay(1000);
-    Serial.print("State: ") ;
-    Serial.println((uint8_t)Si446x_getState(),HEX);
+    Serial1.println("Init: Radio -- Done");
+    delay(1000); // wait after initializing radio
+    Serial1.print("State: 0x") ;
+    Serial1.println((uint8_t)Si446x_getState(),HEX); // get radio state
+    delay(100); // wait
+    Serial1.print("RSSI: 0x");
+    Serial1.println((uint16_t)Si446x_getRSSI(),HEX); // last RSSI
+    Si446x_setupCallback(SI446X_CBS_RXBEGIN, 1); // enable receive interrupt
+    EN_PIPE() ; // Enable PIPE in setup without an actual interrupt, FOR DEBUG ONLY
+//    Serial1.print("TX Status: 0x");Serial1.println(status,HEX);
     delay(100);
-    Serial.print("RSSI: ");
-    Serial.println((uint16_t)Si446x_getRSSI(),HEX);
-    Si446x_setupCallback(SI446X_CBS_RXBEGIN, 1);
-    uint8_t * buf ;
-    Serial.println("Turning on tunnel mode: " ) ;
-    char cmd [] = "ES+W22003320\r" ; //tunnel mode
-    buf = (uint8_t *) malloc (strlen(cmd)+3) ;
-    gen_pack(cmd, strlen(cmd), buf) ;
-    si446x_state_t status ;
-    Si446x_TX(cmd, strlen(cmd), 0, SI446X_STATE_RX);
-    free (buf);
-//    Serial.print("TX Status: 0x");Serial.println(status,HEX);
-    delay(100);
-    while ( true ){
-//      Serial.println("LOOP");
-//      if ( pingInfo.ready ) {
-//        char temp [MAX_PACKET_SIZE] ;
-//        memcpy(temp, (char*)pingInfo.buffer,pingInfo.length);
-//        String recvmsg = (String)temp ;
-//        //Serial.write((uint8_t*)pingInfo.buffer, sizeof(pingInfo.buffer));
-//        Serial.flush();
-//        Serial.print(temp);
-//        pingInfo.ready = PACKET_NONE ;
-//        continue ;
-//      }
-      String msg_str ;
-      while ( Serial.available() <= 0 ) ;
-      msg_str = Serial.readStringUntil('\n');
-      char * msg ;
-      if ( msg_str == "EN+PIPE") {
-        msg_str = "ES+W22003323\r" ;
-      }
-      else
-        msg_str += "\n\r" ;
-      msg = (char * )msg_str.c_str() ;
-      Si446x_TX(msg, strlen(msg), 0, SI446X_STATE_RX);
-//      SI446X_NO_INTERRUPT()
-//      {
-//        // Print the message
-//        Serial.print("TX Status: 0x");Serial.println(status,HEX);
-//      }
-      delay ( 100 ) ;
-    }
 }
+
+/*
+ * There should be a shell script that enables pipe mode first and then creates the PPP
+ * and then enables SSH. This should alleviate frustration.
+ */
 
 void loop ()
 {
-
+  while ( Serial.available() <= 0 ) ; // wait till we have data on the transmission line
+  char msg = Serial.read() ; // read byte
+  Si446x_TX(&msg, sizeof(msg), 0, SI446X_STATE_RX); // transmit byte
+  // delay ( 100 ) ; //Is a delay necessary?
 }
